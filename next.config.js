1e0e51bebee24ae615cc5a831dd4f012dd460afc
Høussaine Amzil <houssaineamzil18@gmail.com>
2022-11-12 18:53:17 +0100

    trying to make this work as wanted

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   typescript: {
     ignoreDevErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dsdlhtnpw/**',
+      },
+    ],
+  },
 }
